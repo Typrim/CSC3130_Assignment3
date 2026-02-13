1a81efea4cfdaad46b7b8f27a9533529327a03b2
Typrim <tommynguyen0531@gmail.com>
2026-02-13 12:23:18 -0600

    Completed Problem 1

---
+++ src/Song.java
@@ -0,0 +1,11 @@
+public class Song {
+    private String title;
+    private String artist;
+    private double length;
+
+    public Song(String title, String artist, double length) {
+        this.title = title;
+        this.artist = artist;
+        this.length = length;
+    }
+}
